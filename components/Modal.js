19ceee29a4b6c50f76e65e2c169610a61fbe6cd2
Bradley Shellnut <bradleyshellnut@pm.me>
2022-11-10 15:26:09 -0600

    Migrate to radix-ui modal.

--- components/Modal.js
+++ components/Modal.js
@@ -1,113 +1,94 @@
-import { DialogContent, DialogOverlay } from '@reach/dialog';
-import VisuallyHidden from '@reach/visually-hidden';
-import '@reach/dialog/styles.css';
+import {
+  Root,
+  Trigger,
+  Portal,
+  Overlay,
+  Content,
+  Title,
+  Description,
+  Close,
+} from '@radix-ui/react-dialog';
 import styled from 'styled-components';
 
-const DialogStyles = styled.div`
+const OverlayStyles = styled.div`
+  background: rgba(0 0 0 / 0.5);
+  position: fixed;
+  top: 0%;
+  left: 0%;
+  right: 0%;
+  bottom: 0%;
+  display: grid;
+  place-items: center;
+  overflow-y: auto;
+`;
+
+const ContentStyles = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  min-width: 300px;
+  background: var(--modalBackground);
+  box-shadow: var(--level-4);
+  padding: 15px;
+  border-radius: var(--borderRadius);
+  width: 50vw;
+
   @media (max-width: 1000px) {
-    div[data-reach-dialog-content] {
-      width: 55vw;
-    }
+    width: 65vw;
   }
   @media (max-width: 800px) {
-    div[data-reach-dialog-content] {
-      width: 60vw;
-    }
+    width: 68vw;
   }
   @media (max-width: 650px) {
-    div[data-reach-dialog-content] {
-      width: 70vw;
-    }
+    width: 70vw;
   }
   @media (max-width: 600px) {
-    div[data-reach-dialog-content] {
-      width: 80vw;
-    }
+    width: 80vw;
   }
   @media (max-width: 550px) {
-    div[data-reach-dialog-content] {
-      width: 90vw;
-    }
+    width: 90vw;
   }
   @media (max-width: 500px) {
-    div[data-reach-dialog-content] {
-      width: 95vw;
-    }
+    width: 95vw;
   }
 `;
-
-const ModalCloseStyles = styled.button`
-  color: var(--black);
-  font-size: 2.5rem;
-  text-shadow: none;
-
-  &:hover {
-    transition: 0.3s ease transform;
-    transform: translate3d(0, -2px, 0);
-  }
-`;
-
-const StandardModalHeader = (props) => {
-  const { onHide, caption } = props;
-  return (
-    <>
-      <div
-        className="modal-header"
-        style={{
-          display: 'flex',
-          flexWrap: 'nowrap',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-        }}
-      >
-        <div className="modal-title">{caption}</div>
-        <ModalCloseStyles
-          type="button"
-          className="close-button"
-          onClick={onHide}
-        >
-          <VisuallyHidden>Close</VisuallyHidden>
-          <span>×</span>
-        </ModalCloseStyles>
-      </div>
-      <hr />
-    </>
-  );
-};
-
 export default function Modal({
   isOpen,
   onHide,
   contentLabel,
   headerCaption,
-  focusRef = null,
   children,
 }) {
   return (
-    <DialogOverlay
-      allowPinchZoom
-      initialFocusRef={focusRef}
-      onDismiss={onHide}
-      isOpen={isOpen}
-    >
-      <DialogStyles>
-        <DialogContent
-          aria-label={contentLabel}
-          style={{
-            background: 'var(--modalBackground)',
-            boxShadow: 'var(--level-4)',
-            borderRadius: 'var(--borderRadius)',
-            maxWidth: '55rem',
-          }}
-        >
-          <div>
-            <div>
-              <StandardModalHeader caption={headerCaption} onHide={onHide} />
-              {children}
-            </div>
-          </div>
-        </DialogContent>
-      </DialogStyles>
-    </DialogOverlay>
+    <Root open={isOpen} onOpenChange={onHide}>
+      <Portal>
+        <OverlayStyles>
+          <Overlay>
+            <ContentStyles>
+              <Content aria-describedby={contentLabel}>
+                {/* <div style={{ display: 'grid', margin: '2rem', borderRadius: '4px'}}> */}
+                <div
+                  style={{
+                    display: 'flex',
+                    flexWrap: 'nowrap',
+                    alignItems: 'center',
+                    justifyContent: headerCaption
+                      ? 'space-between'
+                      : 'flex-end',
+                    marginBottom: '1rem',
+                  }}
+                >
+                  {headerCaption && <Title>{headerCaption}</Title>}
+                  <Close aria-label="Close">x</Close>
+                </div>
+                {children}
+                {/* </div> */}
+              </Content>
+            </ContentStyles>
+          </Overlay>
+        </OverlayStyles>
+      </Portal>
+    </Root>
   );
 }
